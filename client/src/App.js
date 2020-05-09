6ee91f96c183240b339e97814f403b1e26c88536
Mahadevan Sreenivasan <mahadevan_sv@yahoo.com>
2020-05-09 10:38:01 +0530

    feat: Add new event GE_WAIT_FOR_NEXT_ROUND  - fix game loop issues at the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import socket from 'socket.io-client';
-import Canvas from './components/Canvas';
-import { TextField } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import socket from "socket.io-client";
+import Canvas from "./components/Canvas";
+import { TextField } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   const [socketIO, setSocketIO] = useState(null);
   const [drawWord, setDrawWord] = useState(null);
   const [showGuessBox, setShowGuessBox] = useState(false);
-  const [guess, setGuess] = useState('');
+  const [guess, setGuess] = useState("");
   const [currentUser, setCurrentUser] = useState(null);
   const [userScores, setUserScores] = useState(null);
   const [previousWord, setPreviousWord] = useState(null);
 
   useEffect(() => {
-    const io = socket('http://localhost:3001');
-    io.on('connect', () => {
+    const io = socket("http://localhost:3001");
+    io.on("connect", () => {
       const user = {
         id: `player${+new Date()}`,
-        name: 'Player',
+        name: "Player",
       };
-      io.emit('C_S_LOGIN', user);
-      io.on('S_C_LOGIN', () => {
-        console.log('Login success');
+      io.emit("C_S_LOGIN", user);
+      io.on("S_C_LOGIN", () => {
+        console.log("Login success");
         setCurrentUser(user);
       });
 
-      io.on('GE_NEW_GAME', () => {
-        console.log('New Game starting...');
+      io.on("GE_NEW_GAME", () => {
+        console.log("New Game starting...");
       });
 
-      io.on('GE_NEW_ROUND', ({ round, total }) => {
+      io.on("GE_NEW_ROUND", ({ round, total }) => {
         setDrawWord(null);
-        setGuess('');
+        setGuess("");
         setShowGuessBox(true);
+        setPreviousWord(null);
         console.log(`New Round starting, Round: ${round}, Total: ${total}`);
       });
 
-      io.on('GE_WAIT_FOR_NEXT_ROUND', (previousWord) => {
+      io.on("GE_WAIT_FOR_NEXT_ROUND", (previousWord) => {
+        setShowGuessBox(false);
+        setDrawWord(null);
         setPreviousWord(previousWord);
       });
 
-      io.on('GE_ANNOUNCE_WINNER', () => {
+      io.on("GE_ANNOUNCE_WINNER", () => {
+        setShowGuessBox(false);
+        setDrawWord(null);
         console.log(`Announce Winner`);
       });
 
-      io.on('GE_NEW_WORD', (word) => {
+      io.on("GE_NEW_WORD", (word) => {
         setDrawWord(word);
         setShowGuessBox(false);
       });
 
-      io.on('GE_UPDATE_SCORE', (userScores) => {
+      io.on("GE_UPDATE_SCORE", (userScores) => {
         setUserScores(userScores);
         console.table(userScores);
       });
@@ -67,9 +72,12 @@ function App() {
   const guessBoxPressed = (e) => {
     if (e.keyCode === 13) {
       //Enter pressed. send it to server
-      setGuess('');
-      console.log('New guess -', e.target.value);
-      socketIO.emit('GE_NEW_GUESS', { userId: currentUser.id, guess: e.target.value });
+      setGuess("");
+      console.log("New guess -", e.target.value);
+      socketIO.emit("GE_NEW_GUESS", {
+        userId: currentUser.id,
+        guess: e.target.value,
+      });
     }
   };
 
@@ -125,7 +133,9 @@ function App() {
           </Grid>
           {previousWord ? (
             <Grid item xs={12}>
-              <Typography variant="body1">Previous word was {previousWord}</Typography>
+              <Typography variant="body1">
+                Previous word was {previousWord}
+              </Typography>
             </Grid>
           ) : null}
         </Grid>
