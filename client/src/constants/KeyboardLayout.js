bcbcaacfae1c59e04b98d9e5ae91a587ca6c1446
Sampath Kumar Krishnan <sampath3493@gmail.com>
2020-08-05 23:13:04 +0530

    Minor fixes for onscreen keyboard - Change layout of keyboard - Include numbers, upper case letters - Use return and proper backspace keys - this has increased the width of buttons - Addresses #22

---
+++ client/src/constants/KeyboardLayout.js
@@ -0,0 +1,32 @@
+export const OnScreenKeyboardLayout = {
+  default: [
+    'q w e r t y u i o p',
+    'a s d f g h j k l',
+    '{shift} z x c v b n m {backspace}',
+    '{numbers} {space} {ent}'
+  ],
+  shift: [
+    'Q W E R T Y U I O P',
+    'A S D F G H J K L',
+    '{shift} Z X C V B N M {backspace}',
+    '{numbers} {space} {ent}'
+  ],
+  numbers: ['1 2 3', '4 5 6', '7 8 9', '{abc} 0 {backspace}']
+};
+
+export const OnScreenKeyboardDisplay = {
+  '{numbers}': '123',
+  '{ent}': 'return',
+  '{escape}': 'esc ⎋',
+  '{tab}': 'tab ⇥',
+  '{backspace}': '⌫',
+  '{capslock}': 'caps lock ⇪',
+  '{shift}': '⇧',
+  '{controlleft}': 'ctrl ⌃',
+  '{controlright}': 'ctrl ⌃',
+  '{altleft}': 'alt ⌥',
+  '{altright}': 'alt ⌥',
+  '{metaleft}': 'cmd ⌘',
+  '{metaright}': 'cmd ⌘',
+  '{abc}': 'ABC'
+};
